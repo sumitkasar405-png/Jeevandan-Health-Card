1b03f0097a5997cd47d15487b84a352f5dea1452
Sumit Kasar <sumitkasar405@gmail.com>
2026-08-05 15:50:57 +0530

    Added role-based authentication structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RoleSelection } from './modules/auth/role-selection/role-selection';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: RoleSelection
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/modules/auth/role-selection/role-selection.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-selection',
+  imports: [],
+  templateUrl: './role-selection.html',
+  styleUrl: './role-selection.css',
+})
+export class RoleSelection {}
